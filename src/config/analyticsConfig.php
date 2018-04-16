460bf3899c462410355091502fe242547ab1b732
ahmed elsadany <eng.ahmedelsadany1@gmail.com>
2018-04-16 12:37:48 +0200

    Update analyticsConfig.php

--- src/config/analyticsConfig.php
+++ src/config/analyticsConfig.php
@@ -2,5 +2,6 @@
 return[
     'view_id'=>'',//get from google analytics dashboard Admin->viewSettings->View ID
     'extend'=>'',//the path of layout 
-    'ContentArea'=>''//name of content area
+    'ContentArea'=>'',//name of content area
+    'service_path'=>''//the service key path from the root example 'public/service.json'
 ];
